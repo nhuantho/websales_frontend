676009de6ad4e59f063dbd36e895bb5e938275ac
dungdungptit <anhdungvk01@gmail.com>
2022-05-12 01:32:40 +0700

    admin 80% finish

--- src/components/Admin/Container/AddProduct.tsx
+++ src/components/Admin/Container/AddProduct.tsx
@@ -16,29 +16,29 @@ import UploadImage from "./UploadImage";
 // };
 
 type props = {
-    reload : () => void
+    reload: () => void
 };
 type Size = {
-    id : Number
-    size : String
+    id: Number
+    size: String
 }
 // type reload : () =>  void
 
-export default  ({reload} : props) =>  {
-    const [NameProduct, setNameProduct ] = useState("")
-    const [Model, setModel ] = useState("")
-    const [Image, setImage ] = useState([])
-    const [Price, setPrice ] = useState(0)
-    const [Describes, setDescribes ] = useState("")
-    const [Color, setColor ] = useState("")
+export default ({ reload }: props) => {
+    const [NameProduct, setNameProduct] = useState("")
+    const [Model, setModel] = useState("")
+    const [Image, setImage] = useState([])
+    const [Price, setPrice] = useState(0)
+    const [Describes, setDescribes] = useState("")
+    const [Color, setColor] = useState("")
     console.log(Image);
-    
+
 
     const addProduct = () => {
         axios({
             method: "post",
             url: "http://localhost:9191/addProduct",
-            data:{
+            data: {
                 "nameProduct": NameProduct,
                 "model": Model,
                 "image": Image[0],
@@ -46,13 +46,14 @@ export default  ({reload} : props) =>  {
                 "describes": Describes,
                 "color": Color
             },
-          })
-        .then((res) => {reload();
-           alert("San pham vua them co id: " + res.data.id)
-           setIdP(res.data.id)
-           setModalSize(true)
         })
-        .catch((err) => {console.log(err);});
+            .then((res) => {
+                reload();
+                alert("San pham vua them co id: " + res.data.id)
+                setIdP(res.data.id)
+                setModalSize(true)
+            })
+            .catch((err) => { console.log(err); });
     };
 
     const resetAll = () => {
@@ -68,131 +69,135 @@ export default  ({reload} : props) =>  {
 
     //-========================================
     const [modalSize, setModalSize] = useState(false)
-    const [sizes,setSizes] = useState<Size[]>([]);
-    const [selectSize,setSelectSize] = useState(1);
+    const [sizes, setSizes] = useState<Size[]>([]);
+    const [selectSize, setSelectSize] = useState(1);
     const [idP, setIdP] = useState(0);
     const [quantity, setQuantity] = useState(0);
 
     useEffect(() => {
         getAPI("http://localhost:9191/sizes");
-      }, []);
-      const getAPI = (url : string) => {
+    }, []);
+    const getAPI = (url: string) => {
         axios({
-          method: "get",
-          url: url,
-          data: null,
+            method: "get",
+            url: url,
+            data: null,
         })
-          .then((res) => {
-            setSizes(res.data);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
-      };
+            .then((res) => {
+                setSizes(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    };
 
 
-    const addProductSize = () => {    
+    const addProductSize = () => {
         axios({
             method: "post",
             url: "http://localhost:9191/addProductSize",
             data: {
                 "product_id": idP,
-                "size_id" : selectSize,
-                "quantity": quantity, 
+                "size_id": selectSize,
+                "quantity": quantity,
             }
         })
-        .then((res) => {
-            console.log(res.data);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
+            .then((res) => {
+                console.log(res.data);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
         resetAll();
         alert("Them thanh cong")
     }
 
-    const checkSize = (data : Number) => {
-        if(selectSize == data) 
+    const checkSize = (data: Number) => {
+        if (selectSize == data)
             return "red"
         else return "white"
     }
 
 
-  return (
-    <tr>          
-        <td>
-            <input
-                className="input"
-                type="text"
-                placeholder= {NameProduct}
-                onChange={(e) => setNameProduct(e.target.value)}
+    return (
+        <tr>
+            <td>
+                <input
+                    className="input"
+                    type="text"
+                    placeholder={NameProduct}
+                    onChange={(e) => setNameProduct(e.target.value)}
                 >
-            </input>
-        </td>
+                </input>
+            </td>
 
-        <td>
-            <input
-                className="input"
-                type="text"
-                placeholder= {Model}
-                onChange={(e) => setModel(e.target.value)}
+            <td>
+                <input
+                    className="input"
+                    type="text"
+                    placeholder={Model}
+                    onChange={(e) => setModel(e.target.value)}
                 ></input>
-        </td>
-        <td>
-            <input
-                className="input"
-                type="text"
-                placeholder= {Price+""}
-                onChange={(e) => setPrice(Number(e.target.value))}    
+            </td>
+            <td>
+                <input
+                    className="input"
+                    type="text"
+                    placeholder={Price + ""}
+                    onChange={(e) => setPrice(Number(e.target.value))}
                 ></input>
-        </td>
-        <td>
-            <input
-                className="input"
-                type="text"
-                placeholder= {Describes}
-                onChange={(e) => setDescribes(e.target.value)}    
+            </td>
+            <td>
+                <input
+                    className="input"
+                    type="text"
+                    placeholder={Describes}
+                    onChange={(e) => setDescribes(e.target.value)}
                 ></input>
-        </td>
-        <td>
-            <input
-                className="input"
-                type="text"
-                placeholder= {Color}
-                onChange={(e) => setColor(e.target.value)}    
+            </td>
+            <td>
+                <input
+                    className="input"
+                    type="text"
+                    placeholder={Color}
+                    onChange={(e) => setColor(e.target.value)}
                 ></input>
-        </td>
-
-        <button onClick={() => {
-            addProduct()
-        }}
-        >Thêm</button>
-
-        <UploadImage setUploadImage={setImage}/>   
-
-         {/* modal */}
-        {
-            modalSize ?
-            <>
-            <input 
-                className="input"
-                type="text"
-                placeholder= "Quantity"
-                onChange={(e) => setQuantity(Number(e.target.value))} 
-            />
+            </td>
+            
+            <td>
+            <UploadImage setUploadImage={setImage} />
+            </td>
+            <td>
+            {/* modal */}
             {
-                sizes.map((size) => (
-                    <button 
-                        style={{backgroundColor : checkSize(size.id)}}
-                        onClick={() => setSelectSize(Number(size.id))}
-                    >{size.size+""}</button>
-                ))
+                modalSize ?
+                    <>
+                        <input style={{marginBottom: "10px"}}
+                            className="input"
+                            type="number"
+                            placeholder="Quantity"
+                            onChange={(e) => setQuantity(Number(e.target.value))}
+                        />
+                        {
+                            sizes.map((size) => (
+                                <button className='bttn'
+                                    style={{ backgroundColor: checkSize(size.id), margin: "5px" }}
+                                    onClick={() => setSelectSize(Number(size.id))}
+                                >{size.size + ""}</button>
+                            ))
+                        }
+                        <button className='bttn' style={{width: "100%", marginTop: "10px"}} onClick={() => addProductSize()}>Hoàn Thành</button>
+                    </>
+                    :
+                    <></>
             }
-            <button onClick={() => addProductSize()}>Hoàn Thành</button>
-            </>
-            :
-            <></>
-        }
-    </tr>
-  )
+            </td>
+            <td>
+                <button className='bttn' onClick={() => {
+                    addProduct()
+                }}
+                >Thêm</button>
+            </td>
+        </tr>
+    )
 }
